412303341a5b176bf7e9c5bcbd721dc38d44ddac
tenzu-kira <whispers66q@gmail.com>
2025-06-03 12:48:38 +0200

    Create lab6.py

---
+++ 2sem/lab6.py
@@ -0,0 +1,17 @@
+def rk_algorythm(text: str, pattern: str) -> list[int]:
+    result = []
+    patternHash = hash(pattern) #? Python internal hash() function has a constant complexity
+
+    for i in range(len(text) - len(pattern) + 1):
+        textHash = hash(text[i:(len(pattern) + i)])
+        if ((patternHash == textHash) and all([pattern[j] == text[i + j] for j in range(len(pattern))])):
+            result.append(i)
+    return result
+
+if __name__ == "__main__":
+    print(f"Result of rk_algorythm: {rk_algorythm('abca', 'abca')}")
+    print(f"Result of rk_algorythm: {rk_algorythm('abcababdabaaba', 'abaaba')}")
+    print(f"Result of rk_algorythm: {rk_algorythm('abcababdabaab', 'abaaba')}")
+    print(f"Result of rk_algorythm: {rk_algorythm('abcababdabaab', 'ab')}")
+
+#TODO Движение окна на 1 символ, хэширование своё
